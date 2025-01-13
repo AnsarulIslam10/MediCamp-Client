bcf92b2b454f647f0c47d6eec26080973c1b705f
AnsarulIslam10 <ansarulislam01739@gmail.com>
2025-01-13 23:05:38 +0600

    footer

--- src/Layout/MainLayout/MainLayout.jsx
+++ src/Layout/MainLayout/MainLayout.jsx
@@ -1,17 +1,18 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Navbar from '../../components/Shared/Navbar/Navbar';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "../../components/Shared/Navbar/Navbar";
+import Footer from "../../components/Shared/Footer/Footer";
 
 const MainLayout = () => {
-    return (
-        <div>
-            <Navbar></Navbar>
-            <div className='min-h-screen'>
-
-            <Outlet></Outlet>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <Navbar></Navbar>
+      <div className="min-h-[calc(100vh-290px)]">
+        <Outlet></Outlet>
+      </div>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default MainLayout;
---
+++ src/components/Shared/Footer/Footer.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { FaUserDoctor } from "react-icons/fa6";
+import Container from "../Container";
+
+const Footer = () => {
+  return (
+    <footer className="bg-cyan-50 p-10">
+      <Container>
+        <section className="footer">
+          <aside>
+            <FaUserDoctor className="text-8xl text-primary" />
+            <p>
+              Medical Camp Management
+              <br />
+              Providing reliable service since 1992
+            </p>
+          </aside>
+          <nav>
+            <h6 className="footer-title">Services</h6>
+            <a className="link link-hover">Branding</a>
+            <a className="link link-hover">Design</a>
+            <a className="link link-hover">Marketing</a>
+            <a className="link link-hover">Advertisement</a>
+          </nav>
+          <nav>
+            <h6 className="footer-title">Company</h6>
+            <a className="link link-hover">About us</a>
+            <a className="link link-hover">Contact</a>
+            <a className="link link-hover">Jobs</a>
+            <a className="link link-hover">Press kit</a>
+          </nav>
+          <nav>
+            <h6 className="footer-title">Legal</h6>
+            <a className="link link-hover">Terms of use</a>
+            <a className="link link-hover">Privacy policy</a>
+            <a className="link link-hover">Cookie policy</a>
+          </nav>
+        </section>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
