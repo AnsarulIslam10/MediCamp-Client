201af264f7c1ee1c68f9785e3362809549fd97bb
AnsarulIslam10 <ansarulislam01739@gmail.com>
2025-01-13 20:05:28 +0600

    Project client side setup

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'title': '#000000',
+        'description': '#6B7280',
+        'background': '#f5f5f5',
+        'primary': '#01dfdf',
+        'secondary': '#003366',
+        'accent': '#1ddd1d',
+       },
+       
+    },
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+}
