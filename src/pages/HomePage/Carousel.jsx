71d0afb9eb602c5ac89746103d095c84b82e7cfe
AnsarulIslam10 <ansarulislam01739@gmail.com>
2025-01-13 20:59:57 +0600

    banner

---
+++ src/pages/HomePage/Carousel.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-fade";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+// import required modules
+import { EffectFade, Autoplay, Pagination, Navigation } from "swiper/modules";
+import image1 from "../../assets/carousel/Banner 1.webp";
+import image2 from "../../assets/carousel/banner-2.jpg";
+import image3 from "../../assets/carousel/banner 3.jpg";
+
+const Carousel = () => {
+  return (
+    <>
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        effect={"fade"}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+        }}
+        loop={true}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[EffectFade, Autoplay, Pagination, Navigation]}
+        className="mySwiper rounded-xl"
+      >
+        <SwiperSlide>
+          <img
+            className="object-cover h-[30vh] sm:h-[60vh] md:h-[70vh] w-full"
+            src={image1}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            className="object-cover h-[30vh] sm:h-[60vh] md:h-[70vh] w-full"
+            src={image2}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            className="object-cover h-[30vh] sm:h-[60vh] md:h-[70vh] w-full"
+            src={image3}
+          />
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
+};
+
+export default Carousel;
--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Container from '../../components/Shared/Container';
+import Carousel from './Carousel';
 
 const Home = () => {
     return (
         <Container>
-            <h1 className='text-3xl'>Hello world</h1>
+            <Carousel></Carousel>
         </Container>
     );
 };
