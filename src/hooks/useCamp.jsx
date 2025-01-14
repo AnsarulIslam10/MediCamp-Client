a1a8bff8c72fbf3fc86cbad87ee865c3581af863
AnsarulIslam10 <ansarulislam01739@gmail.com>
2025-01-14 10:41:09 +0600

    search functionality

--- src/hooks/useCamp.jsx
+++ src/hooks/useCamp.jsx
@@ -1,15 +1,17 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import { useState } from "react";
 
 const useCamp = () => {
+    const [search, setSearch] = useState('')
     const {data: camp = [], isLoading: loading, refetch} = useQuery({
-        queryKey: ['camp'],
+        queryKey: ['camp', search],
         queryFn: async ()=>{
-            const res = await axios.get('http://localhost:5000/all-camps')
+            const res = await axios.get(`http://localhost:5000/all-camps?search=${search}`)
             return res.data;
         }
     })
-    return [camp, loading, refetch]
+    return [camp, loading, refetch, search, setSearch]
 };
 
 export default useCamp;
--- src/pages/AvailableCamps/AvailableCamps.jsx
+++ src/pages/AvailableCamps/AvailableCamps.jsx
@@ -6,8 +6,15 @@ import Container from "../../components/Shared/Container";
 import { TfiLayoutGrid2Alt, TfiLayoutGrid3Alt } from "react-icons/tfi";
 
 const AvailableCamps = () => {
-  const [camp, loading] = useCamp();
+  const [camp, loading, refetch, search, setSearch] = useCamp();
   const [viewLayout, setViewLayout] = useState("three");
+  const [searchInput, setSearchInput] = useState(search);
+  const handleInputChange = (e) => {
+    setSearchInput(e.target.value);
+  };
+  const handleSearch = () => {
+    setSearch(searchInput);
+  };
   const toggleView = () => {
     setViewLayout((prev) => (prev === "three" ? "two" : "three"));
   };
@@ -17,6 +24,29 @@ const AvailableCamps = () => {
   return (
     <Container>
       <div className="my-16">
+        <label className="input input-bordered flex items-center gap-2 max-w-lg mx-auto">
+          <input
+            value={searchInput}
+            onChange={handleInputChange}
+            type="text"
+            className="grow"
+            placeholder="Search"
+          />
+          <button onClick={handleSearch}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              className="h-4 w-4 opacity-70"
+            >
+              <path
+                fillRule="evenodd"
+                d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </button>
+        </label>
         <div className="flex items-center gap-2  text-3xl justify-end mb-4">
           <button onClick={toggleView}>
             <TfiLayoutGrid2Alt
