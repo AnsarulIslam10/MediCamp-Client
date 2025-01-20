9a1cd84364464a4bcb9d7495fe5201357d87896b
AnsarulIslam10 <ansarulislam01739@gmail.com>
2025-01-20 23:42:01 +0600

    last adjustment

--- src/pages/Dashboard/PaymentHistory/PaymentHistory.jsx
+++ src/pages/Dashboard/PaymentHistory/PaymentHistory.jsx
@@ -31,82 +31,90 @@ const PaymentHistory = () => {
         title={"Payment History"}
         sub={"Track All Your Camp Payments"}
       ></SectionTitle>
-      <div className="flex justify-end mb-2">
-        <label className="input input-bordered flex items-center gap-2">
-          <input
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-            type="text"
-            className="grow"
-            placeholder="Search"
-          />
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 16 16"
-            fill="currentColor"
-            className="h-4 w-4 opacity-70 "
-          >
-            <path
-              fillRule="evenodd"
-              d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-              clipRule="evenodd"
-            />
-          </svg>
-        </label>
-      </div>
-      <div className="overflow-x-auto shadow-card-shadow">
-        <table className="table">
-          {/* head */}
-          <thead>
-            <tr className="bg-primary text-secondary">
-              <th></th>
-              <th>Camp Name</th>
-              <th>Camp Fees</th>
-              <th>Transaction ID</th>
-              <th>Payment Status</th>
-              <th>Confirmation Status</th>
-            </tr>
-          </thead>
-          <tbody>
-            {/* row 1 */}
-            {payments.result?.map((payment, idx) => (
-              <tr key={payment._id} className="hover">
-                <th>{idx + 1}</th>
-                <td>{payment.campName}</td>
-                <td>${payment.campFee}</td>
-                <td className="text-green-600">${payment.transactionId}</td>
-                <td>{payment.status}</td>
-                <td>{payment.confirmationStatus}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-      <div className="flex justify-center items-center mt-6 space-x-4">
-        <button
-          className="btn bg-primary border-none px-6 py-2 rounded-lg shadow-md hover:bg-primary-hover disabled:opacity-50"
-          onClick={() => setPage((prev) => Math.max(prev - 1, 1))}
-          disabled={page === 1}
-        >
-          <span className="text-lg font-semibold">Prev</span>
-        </button>
-        <span className="text-lg font-medium text-gray-700">
-          Page {page} of {payments.totalPages}
-        </span>
-        <button
-          className="btn bg-primary border-none px-6 py-2 rounded-lg shadow-md hover:bg-primary-hover disabled:opacity-50"
-          onClick={() =>
-            setPage((prev) =>
-              payments.totalPages
-                ? Math.min(prev + 1, payments.totalPages)
-                : prev
-            )
-          }
-          disabled={page === payments.totalPages}
-        >
-          <span className="text-lg font-semibold">Next</span>
-        </button>
-      </div>
+      {payments?.result?.length > 0 ? (
+        <>
+          <div className="flex justify-end mb-2">
+            <label className="input input-bordered flex items-center gap-2">
+              <input
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+                type="text"
+                className="grow"
+                placeholder="Search"
+              />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                className="h-4 w-4 opacity-70 "
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </label>
+          </div>
+          <div className="overflow-x-auto shadow-card-shadow">
+            <table className="table">
+              {/* head */}
+              <thead>
+                <tr className="bg-primary text-secondary">
+                  <th></th>
+                  <th>Camp Name</th>
+                  <th>Camp Fees</th>
+                  <th>Transaction ID</th>
+                  <th>Payment Status</th>
+                  <th>Confirmation Status</th>
+                </tr>
+              </thead>
+              <tbody>
+                {/* row 1 */}
+                {payments.result?.map((payment, idx) => (
+                  <tr key={payment._id} className="hover">
+                    <th>{idx + 1}</th>
+                    <td>{payment.campName}</td>
+                    <td>${payment.campFee}</td>
+                    <td className="text-green-600">${payment.transactionId}</td>
+                    <td>{payment.status}</td>
+                    <td>{payment.confirmationStatus}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+          <div className="flex justify-center items-center mt-6 space-x-4">
+            <button
+              className="btn bg-primary border-none px-6 py-2 rounded-lg shadow-md hover:bg-primary-hover disabled:opacity-50"
+              onClick={() => setPage((prev) => Math.max(prev - 1, 1))}
+              disabled={page === 1}
+            >
+              <span className="text-lg font-semibold">Prev</span>
+            </button>
+            <span className="text-lg font-medium text-gray-700">
+              Page {page} of {payments.totalPages}
+            </span>
+            <button
+              className="btn bg-primary border-none px-6 py-2 rounded-lg shadow-md hover:bg-primary-hover disabled:opacity-50"
+              onClick={() =>
+                setPage((prev) =>
+                  payments.totalPages
+                    ? Math.min(prev + 1, payments.totalPages)
+                    : prev
+                )
+              }
+              disabled={page === payments.totalPages}
+            >
+              <span className="text-lg font-semibold">Next</span>
+            </button>
+          </div>
+        </>
+      ) : (
+        <div className="text-3xl sm:text-4xl md:text-5xl font-bold text-red-500 text-center mt-16">
+          No data found
+        </div>
+      )}
     </div>
   );
 };
--- src/pages/Dashboard/RegisteredCamps/RegisteredCamps.jsx
+++ src/pages/Dashboard/RegisteredCamps/RegisteredCamps.jsx
@@ -66,111 +66,119 @@ const RegisteredCamps = () => {
         title={"Registered Camps"}
         sub={"All Your Camp Registrations in One Place"}
       ></SectionTitle>
-      <div className="flex justify-end mb-2">
-        <label className="input input-bordered flex items-center gap-2">
-          <input
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-            type="text"
-            className="grow"
-            placeholder="Search"
-          />
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 16 16"
-            fill="currentColor"
-            className="h-4 w-4 opacity-70 "
-          >
-            <path
-              fillRule="evenodd"
-              d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-              clipRule="evenodd"
-            />
-          </svg>
-        </label>
-      </div>
-      <div className="overflow-x-auto shadow-card-shadow">
-        <table className="table">
-          {/* head */}
-          <thead>
-            <tr className="bg-primary text-secondary">
-              <th></th>
-              <th>Camp Name</th>
-              <th>Camp Fees</th>
-              <th>Participant Name</th>
-              <th>Payment Status</th>
-              <th>Confirmation Status</th>
-              <th>Cancel</th>
-              <th>Feedback</th>
-            </tr>
-          </thead>
-          <tbody>
-            {/* row 1 */}
-            {registeredCamps.result?.map((camp, idx) => (
-              <tr key={camp._id} className="hover">
-                <th>{idx + 1}</th>
-                <td>{camp.campName}</td>
-                <td>${camp.campFees}</td>
-                <td>{camp.participantName}</td>
-                <td>
-                  <Link
-                    disabled={camp.paymentStatus === "paid"}
-                    to={{ pathname: "/dashboard/payment" }}
-                    state={{
-                      campName: camp.campName,
-                      campFees: camp.campFees,
-                      registeredCampId: camp._id,
-                      campId: camp.campId,
-                      confirmationStatus: camp.confirmationStatus,
-                    }}
-                    className={`btn`}
-                  >
-                    {camp.paymentStatus === "unpaid" ? "Pay" : "Paid"}
-                  </Link>
-                </td>
-                <td>{camp.confirmationStatus}</td>
-                <td>
-                  <button
-                    disabled={camp.paymentStatus === "paid"}
-                    className="disabled:text-gray-500 text-xl"
-                    onClick={() => handleDelete(camp._id)}
-                  >
-                    <BiX />
-                  </button>
-                </td>
-                <td>
-                  <FeedbackModal camp={camp}></FeedbackModal>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-      <div className="flex justify-center items-center mt-6 space-x-4">
-        <button
-          className="btn bg-primary border-none px-6 py-2 rounded-lg shadow-md hover:bg-primary-hover disabled:opacity-50"
-          onClick={() => setPage((prev) => Math.max(prev - 1, 1))}
-          disabled={page === 1}
-        >
-          <span className="text-lg font-semibold">Prev</span>
-        </button>
-        <span className="text-lg font-medium text-gray-700">
-          Page {page} of {registeredCamps.totalPages}
-        </span>
-        <button
-          className="btn bg-primary border-none px-6 py-2 rounded-lg shadow-md hover:bg-primary-hover disabled:opacity-50"
-          onClick={() =>
-            setPage((prev) =>
-              registeredCamps.totalPages
-                ? Math.min(prev + 1, registeredCamps.totalPages)
-                : prev
-            )
-          }
-          disabled={page === registeredCamps.totalPages}
-        >
-          <span className="text-lg font-semibold">Next</span>
-        </button>
-      </div>
+      {registeredCamps?.result?.length > 0 ? (
+        <>
+          <div className="flex justify-end mb-2">
+            <label className="input input-bordered flex items-center gap-2">
+              <input
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+                type="text"
+                className="grow"
+                placeholder="Search"
+              />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                className="h-4 w-4 opacity-70 "
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </label>
+          </div>
+          <div className="overflow-x-auto shadow-card-shadow">
+            <table className="table">
+              {/* head */}
+              <thead>
+                <tr className="bg-primary text-secondary">
+                  <th></th>
+                  <th>Camp Name</th>
+                  <th>Camp Fees</th>
+                  <th>Participant Name</th>
+                  <th>Payment Status</th>
+                  <th>Confirmation Status</th>
+                  <th>Cancel</th>
+                  <th>Feedback</th>
+                </tr>
+              </thead>
+              <tbody>
+                {/* row 1 */}
+                {registeredCamps.result?.map((camp, idx) => (
+                  <tr key={camp._id} className="hover">
+                    <th>{idx + 1}</th>
+                    <td>{camp.campName}</td>
+                    <td>${camp.campFees}</td>
+                    <td>{camp.participantName}</td>
+                    <td>
+                      <Link
+                        disabled={camp.paymentStatus === "paid"}
+                        to={{ pathname: "/dashboard/payment" }}
+                        state={{
+                          campName: camp.campName,
+                          campFees: camp.campFees,
+                          registeredCampId: camp._id,
+                          campId: camp.campId,
+                          confirmationStatus: camp.confirmationStatus,
+                        }}
+                        className={`btn`}
+                      >
+                        {camp.paymentStatus === "unpaid" ? "Pay" : "Paid"}
+                      </Link>
+                    </td>
+                    <td>{camp.confirmationStatus}</td>
+                    <td>
+                      <button
+                        disabled={camp.paymentStatus === "paid"}
+                        className="disabled:text-gray-500 text-xl"
+                        onClick={() => handleDelete(camp._id)}
+                      >
+                        <BiX />
+                      </button>
+                    </td>
+                    <td>
+                      <FeedbackModal camp={camp}></FeedbackModal>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+          <div className="flex justify-center items-center mt-6 space-x-4">
+            <button
+              className="btn bg-primary border-none px-6 py-2 rounded-lg shadow-md hover:bg-primary-hover disabled:opacity-50"
+              onClick={() => setPage((prev) => Math.max(prev - 1, 1))}
+              disabled={page === 1}
+            >
+              <span className="text-lg font-semibold">Prev</span>
+            </button>
+            <span className="text-lg font-medium text-gray-700">
+              Page {page} of {registeredCamps.totalPages}
+            </span>
+            <button
+              className="btn bg-primary border-none px-6 py-2 rounded-lg shadow-md hover:bg-primary-hover disabled:opacity-50"
+              onClick={() =>
+                setPage((prev) =>
+                  registeredCamps.totalPages
+                    ? Math.min(prev + 1, registeredCamps.totalPages)
+                    : prev
+                )
+              }
+              disabled={page === registeredCamps.totalPages}
+            >
+              <span className="text-lg font-semibold">Next</span>
+            </button>
+          </div>
+        </>
+      ) : (
+        <div className="text-3xl sm:text-4xl md:text-5xl font-bold text-red-500 text-center mt-16">
+          No data found
+        </div>
+      )}
     </div>
   );
 };
